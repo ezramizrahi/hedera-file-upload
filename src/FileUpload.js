a15c174987cbde3d3e3f388537703d9e2908c22a
ezramizrahi <mizrahi.ez@gmail.com>
2021-02-22 15:48:55 +0800

    first commit

---
+++ src/FileUpload.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function FileUpload() {
+  const [fileData, setFileData] = useState('');
+
+  const getFile = (e) => {
+    setFileData(e.target.files[0]);
+  };
+
+  const uploadFile = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("file", fileData);
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/upload",
+      data: data,
+    }).then((res) => {
+      // we should get the message and topicId here
+      console.log(res.data.message);
+    });
+   };
+
+  return (
+    <form onSubmit={uploadFile}>
+      <input type="file" name="file" onChange={getFile} required />
+      <input type="submit" name="upload" value="Upload" />
+    </form>
+  );
+}
+
+export default FileUpload;
---
+++ src/server.js
@@ -0,0 +1,53 @@
+const { Client, TopicCreateTransaction, TopicMessageSubmitTransaction, Hbar } = require("@hashgraph/sdk");
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+var upload = multer({ dest: "../public/uploads/" });
+
+// keys
+const operatorAccount = '';
+const operatorPrivateKey = '';
+if (operatorAccount == null || operatorPrivateKey == null ) {
+  throw new Error("Environment variables myAccountId and myPrivateKey must be present");
+};
+
+// set up Testnet Client
+const client = Client.forTestnet();
+client.setOperator(operatorAccount, operatorPrivateKey);
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    if (req.file) {
+      // create topic transaction
+      const createResponse = await new TopicCreateTransaction().execute(client);
+      // create receipt
+      const createReceipt = await createResponse.getReceipt(client);
+      console.log(`topic id = ${createReceipt.topicId}`);
+
+      // send one message
+      const sendResponse = await new TopicMessageSubmitTransaction({
+        topicId: createReceipt.topicId,
+        message: req.file.originalname,
+      }).execute(client);
+      const sendReceipt = await sendResponse.getReceipt(client);
+      console.log(`topic sequence number = ${sendReceipt.topicSequenceNumber}`);
+
+      res.send({
+        status: true,
+        message: `Message: ${req.file.originalname}; topicId: ${createReceipt.topicId}`,
+      });
+    } else {
+      res.status(400).send({
+        status: false,
+        data: "File Not Found :(",
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(5000, () => console.log("Server Running..."));
